bdf10a5d2769ca4e1d540b558ef6f85b8fa1add4
Vladimir Inshakov <mr_gogh@mail.ru>
2025-03-29 16:02:01 +0000

    added some description

--- test.sh
+++ test.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # some basic info about the host
+
+# OS version
 lsb_release -a
+
+# Network interfaces info
 ip a
 
+# Processes info
 ps a
 
+# Block and pci devices info
 lsblk
 lspci
 
